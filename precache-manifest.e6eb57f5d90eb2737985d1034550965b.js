b971628883d78c70adf93d97e266cfb4ca1d78d3
fournotfourerror <fournotfourerror@gmail.com>
2020-04-20 12:38:13 +0530

    Updates

---
+++ precache-manifest.e6eb57f5d90eb2737985d1034550965b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30309a2114e1f6ee0621cbf74fd787a0",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "e2b2edad57e840b70ada",
+    "url": "/profile/static/css/main.c0d663da.chunk.css"
+  },
+  {
+    "revision": "443c196c62caa0aeeadf",
+    "url": "/profile/static/js/2.690de6e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile/static/js/2.690de6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b2edad57e840b70ada",
+    "url": "/profile/static/js/main.70e686cd.chunk.js"
+  },
+  {
+    "revision": "182a4400b0e948c43a5c",
+    "url": "/profile/static/js/runtime-main.fd97c823.js"
+  }
+]);
